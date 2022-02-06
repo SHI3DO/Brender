b7fd453bf710af092a9f99411511fe21840a0682
STR-HK <78652371+STR-HK@users.noreply.github.com>
2022-02-06 22:56:20 +0900

    Remove Backend Dev code

--- Brender-backend/Serevere.py
+++ Brender-backend/Serevere.py
@@ -16,17 +16,3 @@ async def root():
 Serevere = Utilities()
 Serevere.load()
 Serevere.cors(app)
-
-# For Dev
-
-if __name__ == "__main__":
-    import uvicorn
-    import socket
-    import os
-
-    main_file = os.path.basename(__file__).removesuffix(".py")
-
-    if socket.gethostname() == "DESKTOP-NI0SO2S":
-        uvicorn.run(f"{main_file}:app", host="192.168.0.2", port=8585, reload=True)
-    else:
-        uvicorn.run(f"{main_file}:app", host="192.168.0.11", port=8586, reload=False)
